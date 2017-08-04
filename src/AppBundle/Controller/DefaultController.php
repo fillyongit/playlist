bb6fb047e07cf13a33ac1951f2645d511a0b00d8
Alessio Filippini <alessio.filippini@janus.it>
2017-08-04 16:09:45 +0200

    Work in progress...

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+    /**
+     * @Route("/list/{what}", name="lista")
+     */
+    public function listAction($what = 'artists')
+    {
+    	// replace this example code with whatever you need
+    	return $this->render('default/list.html.twig', [
+    		'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+    	]);
+    }
 }
