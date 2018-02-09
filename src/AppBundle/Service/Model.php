913ab6a46b2b86c7929fc7fde71561c2cec654ab
Alessio Filippini <alessio.filippini@janus.it>
2018-02-09 17:54:08 +0100

    Work in progress...

--- src/AppBundle/Service/Model.php
+++ src/AppBundle/Service/Model.php
@@ -15,8 +15,8 @@ class Model {
 		$this->em = $em;
 	}
 	
-	public function getEntity($entityFullName) {
-		return $this->em->getRepository($entityFullName)->findAll();
+	public function getEntity($entityFullName, $id) {
+		return $this->em->getRepository($entityFullName)->find($id);
 	}
 	
 	public function getCollection($entityFullName) {
