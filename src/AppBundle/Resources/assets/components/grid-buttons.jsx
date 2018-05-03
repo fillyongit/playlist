7c3e4084929a3d3d5335a030160851038b2975c4
Alessio Filippini <alessio.filippini@janus.it>
2018-05-03 17:36:40 +0200

    Work in progress...

--- src/AppBundle/Resources/assets/components/grid-buttons.jsx
+++ src/AppBundle/Resources/assets/components/grid-buttons.jsx
@@ -70,8 +70,10 @@ class GridButtons extends React.Component {
 		return (
       <div>
         <div className="grid-buttons">
-          {app.userIsEditor ? (<i className="btn fas fa-pencil-alt fa-lg" onClick={this.editArtist}></i>) : ''}
-          <i className="btn fas fa-trash fa-lg"></i>
+          {app.userIsEditor ? (
+            <i className="btn fas fa-pencil-alt fa-lg" onClick={this.editArtist}></i>
+            <i className="btn fas fa-trash fa-lg"></i>
+          ) : ''}
           <i className={'btn fas ' + (this.state.isPlayButtonToggleOn ? 'fa-headphones' : 'fa-play') + ' fa-lg'} 
             onClick={this.playArtistList}></i>
         </div>
